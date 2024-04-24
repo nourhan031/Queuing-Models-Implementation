30038d0b97154dc51e591ffa964ed17db366346a
nourhan031 <nourhanabdallah031@gmail.com>
2024-04-25 01:36:28 +0200

    skeleton

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-print("hello")
+# lecture 5
+def MM_1():
+    # M: Poisson arrival process
+    # M: exponential service time distribution
+    # 1: one server
+
+    # Discipline: FCFS
+
+
+    print("MM1 Queuing Model")
+
+# lecture 6
+def MM_infinity():
+    print("MM-infinity Queuing Model")
+
+# lecture 7
+def MM1_k(): # finite storage
+
+    print("MM1k Queuing Model")
+
+def MM1_m(): # finite cutomer population
+    print("MM1m Queuing Moddel")
+
+def main():
+    # Get user input for characteristics
+
+    # arrivals = input("Enter the probability distribution of arrivals: ")
+    # service_time = input("Enter the probability distribution of service time: ")
+    # servers = input("Enter the number of servers: ")
+    # system_capacity = input("Enter system capacity: ")
+    # pop_size = input("Enter population size: ")
+    # discipline = input("Enter system discipline: ")
+    # ...
+    # Based on the input, call the appropriate function
+
+    # if arrivals == 'M' and service_time == 'M' and servers == '1':
+    #     MM_1()
+    # elif arrivals == 'M' and service_time == 'M' and servers == 'infinity':
+    #     MM_infinity()
+    # ...
+
+if __name__ == "__main__":
+    main()
